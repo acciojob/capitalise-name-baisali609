d177c0d0151040b108f532b0a12dbc160977ed55
baisali609 <baisali609@gmail.com>
2025-02-13 12:43:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //your JS code here. If required.
-function uppercase() {
-            let input = document.getElementById("fname");
-	btn.addEventListener("blur",()=>{
-		input.value = input.value.toUpperCase();
-})          
-}
+let input = document.getElementById("fname");
+
+input.addEventListener("blur",()=>{
+	input.value = input.value.toUpperCase();
+});
