28edb734dfbea99c9014ee4ed767cfb4f684a245
baisali609 <baisali609@gmail.com>
2025-02-13 09:09:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+function uppercase() {
+            let input = document.getElementById("#fname");
+            input.value = input.value.toUpperCase();
+        }
